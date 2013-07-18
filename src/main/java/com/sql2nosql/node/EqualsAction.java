ef5a25debca717a78fffde3af9e9ba97ad96decb
ormanli <serdar.ormanli@gmail.com>
2013-07-18 22:12:14 +0300

    Nodes added.

---
+++ src/main/java/com/sql2nosql/node/EqualsAction.java
@@ -0,0 +1,9 @@
+package com.sql2nosql.node;
+
+import com.akiban.sql.parser.QueryTreeNode;
+
+public class EqualsAction implements NodeAction {
+    @Override
+    public void action(QueryTreeNode node) {
+    }
+}
---
+++ src/main/java/com/sql2nosql/node/GreaterAction.java
@@ -0,0 +1,9 @@
+package com.sql2nosql.node;
+
+import com.akiban.sql.parser.QueryTreeNode;
+
+public class GreaterAction implements NodeAction {
+    @Override
+    public void action(QueryTreeNode node) {
+    }
+}

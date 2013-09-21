5ca57214246d542162b5b4a4e9bc12e30b46ca28
ormanli <serdar.ormanli@gmail.com>
2013-09-21 23:00:50 +0300

    Copyright added.

--- src/main/java/com/sql2nosql/QueryTreeVisitor.java
+++ src/main/java/com/sql2nosql/QueryTreeVisitor.java
@@ -1,13 +1,14 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+/*******************************************************************************
+ * Copyright (c) 2013 Serdar Ormanlı.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * 
+ * Contributors:
+ *     Serdar Ormanlı - initial API and implementation
+ ******************************************************************************/
 package com.sql2nosql;
-
-/**
- *
- * @author ormanli
- */
 
 import com.akiban.sql.StandardException;
 import com.akiban.sql.parser.*;
--- src/main/java/com/sql2nosql/util/BiMap.java
+++ src/main/java/com/sql2nosql/util/BiMap.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Serdar Ormanlı.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * 
+ * Contributors:
+ *     Serdar Ormanlı - initial API and implementation
+ ******************************************************************************/
 package com.sql2nosql.util;
 
 import java.util.HashMap;

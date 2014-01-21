bb5c6c8aa44661eddef499de978d5a97d8027dee
ormanli <serdar.ormanli@gmail.com>
2014-01-21 21:21:21 +0200

    settings changed

--- src/main/java/com/sql2nosql/SettingsImporter.java
+++ src/main/java/com/sql2nosql/SettingsImporter.java
@@ -10,37 +10,19 @@
  ******************************************************************************/
 package com.sql2nosql;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
-import java.util.HashMap;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
+
+import com.sql2nosql.util.Settings;
 
 public class SettingsImporter {
 
-    public static HashMap<String, Object> importSettings(String path) throws Exception {
-        HashMap<String, Object> output = new HashMap<String, Object>();
-
-        File fXmlFile = new File(path);
-        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-        Document doc = dBuilder.parse(fXmlFile);
-
-        doc.getDocumentElement().normalize();
-
-        NodeList nList = doc.getElementsByTagName("setting");
-
-        for (int temp = 0; temp < nList.getLength(); temp++) {
-            Node nNode = nList.item(temp);
-            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-                Element eElement = (Element) nNode;
-                output.put(eElement.getElementsByTagName("key").item(0).getTextContent(), eElement.getElementsByTagName("value").item(0).getTextContent());
-            }
-        }
-        return output;
-    }
+	public static Settings importSettings(String path) throws Exception {
+		File file = new File(path);
+		JAXBContext jaxbContext = JAXBContext.newInstance(Settings.class);
+		Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+		return (Settings) jaxbUnmarshaller.unmarshal(file);
+	}
 }
